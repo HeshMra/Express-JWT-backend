157e44cf7c0328c66b9b2d1bcb7d27e3acbe4b38
heshith <heshith619@gmail.com>
2025-01-06 13:58:24 +0530

    comment

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// Define the User schema
+// Define the User schemaa
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
