5cd7a68c148ce8eda079b355dc7fcba8c3e9ff40
heshith <heshith619@gmail.com>
2025-01-07 10:27:32 +0530

    enable cors

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+const cors = require('cors');
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 app.use(bodyParser.json()); // Parse JSON request bodies
+// Enable CORS for all routes
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
